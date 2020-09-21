b42ba530c05060b54d586fc47da1b5fd31cf1c63
angelkjh7 <angelkjh5@naver.com>
2020-09-21 23:10:24 +0900

    ddd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("222222222");
 		System.out.println("4444444");
 		System.out.println("33333333333");
 		System.out.println("Hello World");
