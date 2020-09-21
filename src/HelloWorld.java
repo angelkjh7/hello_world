6bd150e52e8d6b6ccc5035dee7d08aca1c966f07
angelkjh7 <angelkjh5@naver.com>
2020-09-21 21:15:50 +0900

    aa

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,9 +4,14 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hi world1");
+		System.out.println("Hi Eclipse1");
 		System.out.println("Hi world2");
+		System.out.println("Hi Eclipse2");
 		System.out.println("Hi world3");
+		System.out.println("Hi Eclipse3");
 		System.out.println("Hi world4");
+		System.out.println("Hi Eclipse4");
 		System.out.println("Hi world5");
+		System.out.println("Hi Eclipse5");
 	}
 }
